53c6869d67efce83f2848a6e83cb853de85fca0a
Adarsh Srivastava <adarshs@iitk.ac.in>
2017-06-24 23:06:16 +0530

    Solution to Assignment-2 Added

--- Assignment-2/Assignment2.java
+++ Assignment-2/Assignment2.java
@@ -1,251 +1,188 @@
 import java.util.Scanner;
 
+public class Assignment2 {
 
-/* Class Node */
-class RedBlackNode
-{
-    RedBlackNode left, right, parent1;
-    int element;
-    int color;
+    private final int RED = 0;
+    private final int BLACK = 1;
 
-    /* Constructor */
-    public RedBlackNode(int theElement)
-    {
-        this( theElement, null, null );
-    }
-    /* Constructor */
-    public RedBlackNode(int theElement, RedBlackNode lt, RedBlackNode rt)
-    {
-        left = lt;
-        right = rt;
-        element = theElement;
-        color = 1;
-    }
-}
+    private class Node {
 
-/* Class RBTree */
-class RBTree
-{
-    private RedBlackNode current;
-    private RedBlackNode parent;
-    private RedBlackNode grand;
-    private RedBlackNode great;
-    private RedBlackNode header;
-    private static RedBlackNode nullNode;
-    /* static initializer for nullNode */
-    static
-    {
-        nullNode = new RedBlackNode(0);
-        nullNode.left = nullNode;
-        nullNode.right = nullNode;
-    }
-    static final int BLACK = 1;
-    static final int RED   = 0;
+        int key = -1, color = BLACK;
+        Node left = nil, right = nil, parent = nil;
 
+        Node(int key) {
+            this.key = key;
+        } 
+    }
 
+    private final Node nil = new Node(-1); 
+    private Node root = nil;
 
-    public RBTree(int negInf)
-    {
-        header = new RedBlackNode(negInf);
-        header.left = nullNode;
-        header.right = nullNode;
-    }
-    /* Function to check if tree is empty */
-    public boolean isEmpty()
-    {
-        return header.right == nullNode;
-    }
-    /* Make the tree logically empty */
-    public void makeEmpty()
-    {
-        header.right = nullNode;
-    }
-    /* Function to insert item */
-    public void insert(int item )
-    {
-        current = parent = grand = header;
-        nullNode.element = item;
-        while (current.element != item)
-        {
-            great = grand;
-            grand = parent;
-            parent = current;
-            current = item < current.element ? current.left : current.right;
-            // Check if two red children and fix if so
-            if (current.left.color == RED && current.right.color == RED)
-                handleReorient( item );
-        }
-        // Insertion fails if already present
-        if (current != nullNode)
+    public void printTree(Node node) {
+        if (node == nil) {
             return;
-        current = new RedBlackNode(item, nullNode, nullNode);
-        // Attach to parent
-        if (item < parent.element) {
-            parent.left = current;
-            current.parent1 = parent;
         }
-        else {
-            parent.right = current;
-            current.parent1 = parent;
-        }
-        handleReorient( item );
-    }
-    private void handleReorient(int item)
-    {
-        // Do the color flip
-        current.color = RED;
-        current.left.color = BLACK;
-        current.right.color = BLACK;
-
-        if (parent.color == RED)
-        {
-            // Have to rotate
-            grand.color = RED;
-            if (item < grand.element != item < parent.element)
-                parent = rotate( item, grand );  // Start dbl rotate
-            current = rotate(item, great );
-            current.color = BLACK;
-        }
-        // Make root black
-        header.right.color = BLACK;
-    }
-    private RedBlackNode rotate(int item, RedBlackNode parent)
-    {
-        if(item < parent.element)
-            return parent.left = item < parent.left.element ? rotateWithLeftChild(parent.left) : rotateWithRightChild(parent.left) ;
-        else
-            return parent.right = item < parent.right.element ? rotateWithLeftChild(parent.right) : rotateWithRightChild(parent.right);
-    }
-    /* Rotate binary tree node with left child */
-    private RedBlackNode rotateWithLeftChild(RedBlackNode k2)
-    {
-        RedBlackNode k1 = k2.left;
-        k2.left = k1.right;
-        k1.right = k2;
-        return k1;
-    }
-    /* Rotate binary tree node with right child */
-    private RedBlackNode rotateWithRightChild(RedBlackNode k1)
-    {
-        RedBlackNode k2 = k1.right;
-        k1.right = k2.left;
-        k2.left = k1;
-        return k2;
-    }
-    /* Functions to count number of nodes */
-    public int countNodes()
-    {
-        return countNodes(header.right);
-    }
-    private int countNodes(RedBlackNode r)
-    {
-        if (r == nullNode)
-            return 0;
-        else
-        {
-            int l = 1;
-            l += countNodes(r.left);
-            l += countNodes(r.right);
-            return l;
-        }
-    }
-    /* Functions to search for an element */
-    public boolean search(int val)
-    {
-        return search(header.right, val);
-    }
-    private boolean search(RedBlackNode r, int val)
-    {
-        boolean found = false;
-        while ((r != nullNode) && !found)
-        {
-            int rval = r.element;
-            if (val < rval)
-                r = r.left;
-            else if (val > rval)
-                r = r.right;
-            else
-            {
-                found = true;
-                break;
-            }
-            found = search(r, val);
-        }
-        return found;
-    }
-
-    /* Function for preorder traversal */
-    public void preorder()
-    {
-        preorder(header.right);
-    }
-    private void preorder(RedBlackNode r)
-    {
-        if (r != nullNode)
-        {
-            char c = 'B';
-            if (r.color == 0)
-                c = 'R';
-            System.out.print(r.element +""+c+" "+parent.element);
-            preorder(r.left);
-            preorder(r.right);
-        }
-    }
-
-}
-
-/* Class RedBlackTreeTest */
-public class Assignment2
-{
-    public static void main(String[] args)
-    {
-        Scanner scan = new Scanner(System.in);
-        /* Creating object of RedBlack Tree */
-        RBTree rbt = new RBTree(Integer.MIN_VALUE);
-        System.out.println("Red Black Tree Test\n");
+        printTree(node.left);
         char ch;
-        /*  Perform tree operations  */
-        do
-        {
-            System.out.println("\nRed Black Tree Operations\n");
-            System.out.println("1. insert ");
-            System.out.println("2. search");
-            System.out.println("3. count nodes");
-            System.out.println("4. check empty");
-            System.out.println("5. clear tree");
+        if (node.color == RED)
+            ch = 'R';
+        else
+            ch = 'B';
+        System.out.print(node.key+" "+ch+" ");
+        if (node.parent.key == -1)
+            System.out.print("NIL");
+        else
+            System.out.print(node.parent.key);
+        System.out.println();
+        
+        printTree(node.right);
+    }
 
-            int choice = scan.nextInt();
-            switch (choice)
-            {
-                case 1 :
-                    System.out.println("Enter integer element to insert");
-                    rbt.insert( scan.nextInt() );
-                    break;
-                case 2 :
-                    System.out.println("Enter integer element to search");
-                    System.out.println("Search result : "+ rbt.search( scan.nextInt() ));
-                    break;
-                case 3 :
-                    System.out.println("Nodes = "+ rbt.countNodes());
-                    break;
-                case 4 :
-                    System.out.println("Empty status = "+ rbt.isEmpty());
-                    break;
-                case 5 :
-                    System.out.println("\nTree Cleared");
-                    rbt.makeEmpty();
-                    break;
-                default :
-                    System.out.println("Wrong Entry \n ");
-                    break;
+    private void insert(Node node) {
+        Node temp = root;
+        if (root == nil) {
+            root = node;
+            node.color = BLACK;
+            node.parent = nil;
+        } else {
+            node.color = RED;
+            while (true) {
+                if (node.key < temp.key) {
+                    if (temp.left == nil) {
+                        temp.left = node;
+                        node.parent = temp;
+                        break;
+                    } else {
+                        temp = temp.left;
+                    }
+                } else if (node.key >= temp.key) {
+                    if (temp.right == nil) {
+                        temp.right = node;
+                        node.parent = temp;
+                        break;
+                    } else {
+                        temp = temp.right;
+                    }
+                }
+            }
+            fixTree(node);
+        }
+    }
+
+    //Takes as argument the newly inserted node
+    private void fixTree(Node node) {
+        while (node.parent.color == RED) {
+            Node uncle = nil;
+            if (node.parent == node.parent.parent.left) {
+                uncle = node.parent.parent.right;
+
+                if (uncle != nil && uncle.color == RED) {
+                    node.parent.color = BLACK;
+                    uncle.color = BLACK;
+                    node.parent.parent.color = RED;
+                    node = node.parent.parent;
+                    continue;
+                } 
+                if (node == node.parent.right) {
+                    //Double rotation needed
+                    node = node.parent;
+                    rotateLeft(node);
+                } 
+                node.parent.color = BLACK;
+                node.parent.parent.color = RED;
+                //if the "else if" code hasn't executed, this
+                //is a case where we only need a single rotation 
+                rotateRight(node.parent.parent);
+            } else {
+                uncle = node.parent.parent.left;
+                 if (uncle != nil && uncle.color == RED) {
+                    node.parent.color = BLACK;
+                    uncle.color = BLACK;
+                    node.parent.parent.color = RED;
+                    node = node.parent.parent;
+                    continue;
+                }
+                if (node == node.parent.left) {
+                    //Double rotation needed
+                    node = node.parent;
+                    rotateRight(node);
+                }
+                node.parent.color = BLACK;
+                node.parent.parent.color = RED;
+                //if the "else if" code hasn't executed, this
+                //is a case where we only need a single rotation
+                rotateLeft(node.parent.parent);
+            }
+        }
+        root.color = BLACK;
+    }
+
+    void rotateLeft(Node node) {
+        if (node.parent != nil) {
+            if (node == node.parent.left) {
+                node.parent.left = node.right;
+            } else {
+                node.parent.right = node.right;
+            }
+            node.right.parent = node.parent;
+            node.parent = node.right;
+            if (node.right.left != nil) {
+                node.right.left.parent = node;
+            }
+            node.right = node.right.left;
+            node.parent.left = node;
+        } else {//Need to rotate root
+            Node right = root.right;
+            root.right = right.left;
+            right.left.parent = root;
+            root.parent = right;
+            right.left = root;
+            right.parent = nil;
+            root = right;
+        }
+    }
+
+    void rotateRight(Node node) {
+        if (node.parent != nil) {
+            if (node == node.parent.left) {
+                node.parent.left = node.left;
+            } else {
+                node.parent.right = node.left;
             }
 
-            System.out.print("\nPre order : ");
-            rbt.preorder();
-
-
-            System.out.println("\nDo you want to continue (Type y or n) \n");
-            ch = scan.next().charAt(0);
-        } while (ch == 'Y'|| ch == 'y');
+            node.left.parent = node.parent;
+            node.parent = node.left;
+            if (node.left.right != nil) {
+                node.left.right.parent = node;
+            }
+            node.left = node.left.right;
+            node.parent.right = node;
+        } else {//Need to rotate root
+            Node left = root.left;
+            root.left = root.left.right;
+            left.right.parent = root;
+            root.parent = left;
+            left.right = root;
+            left.parent = nil;
+            root = left;
+        }
+    }
+    
+    public void consoleUI() {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+        Node node;
+        for (int i=0;i<n;i++){
+            a[i] = scan.nextInt();
+            node = new Node(a[i]);
+            insert(node);
+        }
+        printTree(root);
+        
+    }
+    public static void main(String[] args) {
+        Assignment2 rbt = new Assignment2();
+        rbt.consoleUI();
     }
 }
